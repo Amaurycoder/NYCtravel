98ab8e437029d8e31f11444e2d555c11eb1feee1
Amaury Michaux <amaury.michaux@enpc.fr>
2026-04-15 00:15:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,34 @@
-const CACHE_NAME = 'nyc-focus-v1';
+const CACHE_NAME = 'nyc-focus-v5';
 const urlsToCache = [
+  './',
   './index.html',
   './manifest.json',
   './icon.svg'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
